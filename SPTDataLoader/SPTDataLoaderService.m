a9b87518f5fa8d2f695a216bda3da58ec51701a8
Will Sackfield <sackfield@spotify.com>
2014-11-10 14:52:04 +0100

    SPTDataLoader: Use NSURLSession in service

--- SPTDataLoader/SPTDataLoaderService.m
+++ SPTDataLoader/SPTDataLoaderService.m
@@ -1,5 +1,18 @@
 #import "SPTDataLoaderService.h"
 
+#import "SPTDataLoader+Private.h"
+#import "SPTDataLoaderFactory+Private.h"
+#import "SPTCancellationTokenFactoryImplementation.h"
+#import "SPTCancellationToken.h"
+
+@interface SPTDataLoaderService () <SPTDataLoaderPrivateDelegate, SPTCancellationTokenDelegate, NSURLSessionDelegate>
+
+@property (nonatomic, strong) id<SPTCancellationTokenFactory> cancellationTokenFactory;
+@property (nonatomic, strong) NSURLSession *session;
+@property (nonatomic, strong) NSOperationQueue *sessionQueue;
+
+@end
+
 @implementation SPTDataLoaderService
 
 #pragma mark SPTDataLoaderService
@@ -11,16 +24,50 @@
 
 - (instancetype)initWithUserAgent:(NSString *)userAgent
 {
+    const NSTimeInterval SPTDataLoaderServiceTimeoutInterval = 20.0;
+    
+    NSString * const SPTDataLoaderServiceUserAgentHeader = @"User-Agent";
+    
     if (!(self = [super init])) {
         return nil;
     }
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    configuration.timeoutIntervalForRequest = SPTDataLoaderServiceTimeoutInterval;
+    configuration.timeoutIntervalForResource = SPTDataLoaderServiceTimeoutInterval;
+    configuration.HTTPShouldUsePipelining = YES;
+    if (userAgent) {
+        configuration.HTTPAdditionalHeaders = @{ SPTDataLoaderServiceUserAgentHeader : userAgent };
+    }
+    
+    _cancellationTokenFactory = [SPTCancellationTokenFactoryImplementation new];
+    _sessionQueue = [NSOperationQueue new];
+    _sessionQueue.maxConcurrentOperationCount = 1;
+    _sessionQueue.name = NSStringFromClass(self.class);
+    _session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:_sessionQueue];
     
     return self;
 }
 
 - (SPTDataLoaderFactory *)createDataLoaderFactory
 {
-    return nil;
+    return [SPTDataLoaderFactory dataLoaderFactoryWithPrivateDelegate:self];
 }
+
+#pragma mark SPTDataLoaderPrivateDelegate
+
+- (id<SPTCancellationToken>)performRequest:(SPTDataLoaderRequest *)request
+{
+    return [self.cancellationTokenFactory createCancellationTokenWithDelegate:self];
+}
+
+#pragma mark SPTCancellationTokenDelegate
+
+- (void)cancellationTokenDidCancel:(id<SPTCancellationToken>)cancellationToken
+{
+    
+}
+
+#pragma mark NSURLSessionDelegate
 
 @end
