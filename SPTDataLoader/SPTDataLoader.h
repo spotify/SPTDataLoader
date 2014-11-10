ef04e23038779b0b92d46fa15b73d8bc2c2719eb
Will Sackfield <sackfield@spotify.com>
2014-11-10 11:08:54 +0100

    SPTDataLoader: Add base project

---
+++ SPTDataLoader/SPTDataLoader.h
@@ -0,0 +1,13 @@
+//
+//  SPTDataLoader.h
+//  SPTDataLoader
+//
+//  Created by Will Sackfield on 2014-11-10.
+//  Copyright (c) 2014 spotify. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPTDataLoader : NSObject
+
+@end
---
+++ SPTDataLoader/SPTDataLoader.m
@@ -0,0 +1,13 @@
+//
+//  SPTDataLoader.m
+//  SPTDataLoader
+//
+//  Created by Will Sackfield on 2014-11-10.
+//  Copyright (c) 2014 spotify. All rights reserved.
+//
+
+#import "SPTDataLoader.h"
+
+@implementation SPTDataLoader
+
+@end
