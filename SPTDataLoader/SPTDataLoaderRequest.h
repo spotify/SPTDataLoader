1b2cb16b0d2226540885657d5ffab8b07baa6c9c
Will Sackfield <sackfield@spotify.com>
2014-11-10 11:35:10 +0100

    SPTDataLoader: Add SPTDataLoaderRequest

---
+++ SPTDataLoader/SPTDataLoaderRequest.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+/**
+ * A representing of the request to make to the backend
+ */
+@interface SPTDataLoaderRequest : NSObject
+
+@end
---
+++ SPTDataLoader/SPTDataLoaderRequest.m
@@ -0,0 +1,5 @@
+#import "SPTDataLoaderRequest.h"
+
+@implementation SPTDataLoaderRequest
+
+@end
