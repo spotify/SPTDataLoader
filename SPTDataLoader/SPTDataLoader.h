05d6c80cd1890060a00b1b62a3f93fc72c914a60
Will Sackfield <sackfield@spotify.com>
2014-11-10 11:33:02 +0100

    SPTDataLoader: Add SPTDataLoader

--- SPTDataLoader/SPTDataLoader.h
+++ SPTDataLoader/SPTDataLoader.h
@@ -1,13 +1,59 @@
-//
-//  SPTDataLoader.h
-//  SPTDataLoader
-//
-//  Created by Will Sackfield on 2014-11-10.
-//  Copyright (c) 2014 spotify. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
+@class SPTDataLoaderRequest;
+@class SPTDataLoaderResponse;
+@class SPTDataLoader;
+
+@protocol SPTCancellationToken;
+
+/**
+ * The protocol used for listening to the result of performing requests on the SPTDataLoader
+ * @discussion One of the following callbacks are guaranteed to happen for every request being track by the data loader:
+ * - didReceiveSuccessfulResponse
+ * - didReceiveErrorResponse
+ * - didCancelRequest
+ */
+@protocol SPTDataLoaderDelegate <NSObject>
+
+/**
+ * Called when the data loader received a successful response
+ * @param dataLoader The data loader that received the successful response
+ * @param response The object describing the response
+ */
+- (void)dataLoader:(SPTDataLoader *)dataLoader didReceiveSuccessfulResponse:(SPTDataLoaderResponse *)response;
+/**
+ * Called when the data loader received an error response
+ * @param dataLoader The data loader that received the error response
+ * @param response The object describing the response
+ */
+- (void)dataLoader:(SPTDataLoader *)dataLoader didReceiveErrorResponse:(SPTDataLoaderResponse *)response;
+/**
+ * Called when the data loader cancels a request
+ * @param dataLoader The data loader that cancelled the request
+ * @param request The object describing the request that was cancelled
+ */
+- (void)dataLoader:(SPTDataLoader *)dataLoader didCancelRequest:(SPTDataLoaderRequest *)request;
+
+@end
+
+/**
+ * The object used for performing requests
+ */
 @interface SPTDataLoader : NSObject
+
+/**
+ * The object listening to the data loader
+ */
+@property (nonatomic, weak) id<SPTDataLoaderDelegate> delegate;
+
+/**
+ * Performs a request
+ * @param request The object describing the kind of request to be performed
+ */
+- (id<SPTCancellationToken>)performRequest:(SPTDataLoaderRequest *)request;
+/**
+ * Cancels all the currently operating and pending requests
+ */
+- (void)cancelAllLoads;
 
 @end
--- SPTDataLoader/SPTDataLoader.m
+++ SPTDataLoader/SPTDataLoader.m
@@ -1,13 +1,16 @@
-//
-//  SPTDataLoader.m
-//  SPTDataLoader
-//
-//  Created by Will Sackfield on 2014-11-10.
-//  Copyright (c) 2014 spotify. All rights reserved.
-//
-
 #import "SPTDataLoader.h"
 
 @implementation SPTDataLoader
+
+#pragma mark SPTDataLoader
+
+- (id<SPTCancellationToken>)performRequest:(SPTDataLoaderRequest *)request
+{
+    return nil;
+}
+
+- (void)cancelAllLoads
+{
+}
 
 @end
