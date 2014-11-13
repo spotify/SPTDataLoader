dd767fc9d0a4a1a5f5763af34201cfe0719123e1
Will Sackfield <sackfield@spotify.com>
2014-11-13 18:13:08 +0100

    SPTDataLoader: Add test for relaying data reception to request response handler

--- SPTDataLoaderTests/SPTDataLoaderRequestOperationTest.m
+++ SPTDataLoaderTests/SPTDataLoaderRequestOperationTest.m
@@ -2,9 +2,13 @@
 
 #import "SPTDataLoaderRequestOperation.h"
 
+#import "SPTDataLoaderRequestResponseHandlerMock.h"
+
 @interface SPTDataLoaderRequestOperationTest : XCTestCase
 
 @property (nonatomic, strong) SPTDataLoaderRequestOperation *operation;
+
+@property (nonatomic, strong) SPTDataLoaderRequestResponseHandlerMock *requestResponseHandler;
 
 @end
 
@@ -16,9 +20,10 @@
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.requestResponseHandler = [SPTDataLoaderRequestResponseHandlerMock new];
     self.operation = [SPTDataLoaderRequestOperation dataLoaderRequestOperationWithRequest:nil
                                                                                      task:nil
-                                                                   requestResponseHandler:nil
+                                                                   requestResponseHandler:self.requestResponseHandler
                                                                               rateLimiter:nil];
 }
 
@@ -33,6 +38,12 @@
 - (void)testNotNil
 {
     XCTAssertNotNil(self.operation, @"The operation should not be nil after its construction");
+}
+
+- (void)testReceiveDataRelayedToRequestResponseHandler
+{
+    [self.operation receiveData:nil];
+    XCTAssertEqual(self.requestResponseHandler.numberOfReceivedDataRequestCalls, 1, @"The operation did not relay the received data onto its request response handler");
 }
 
 @end
