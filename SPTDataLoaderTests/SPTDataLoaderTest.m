467349beb2b9bf0284931c98608799b3670150b6
Will Sackfield <sackfield@spotify.com>
2014-11-13 15:29:01 +0100

    SPTDataLoader: Add test for relaying perform request to response handler delegate

--- SPTDataLoaderTests/SPTDataLoaderTest.m
+++ SPTDataLoaderTests/SPTDataLoaderTest.m
@@ -2,11 +2,15 @@
 
 #import <SPTDataLoader/SPTDataLoader.h>
 
+#import <SPTDataLoader/SPTDataLoaderRequest.h>
+
 #import "SPTDataLoader+Private.h"
+#import "SPTDataLoaderRequestResponseHandlerDelegateMock.h"
 
 @interface SPTDataLoaderTest : XCTestCase
 
 @property (nonatomic, strong) SPTDataLoader *dataLoader;
+@property (nonatomic, strong) SPTDataLoaderRequestResponseHandlerDelegateMock *delegate;
 
 @end
 
@@ -18,7 +22,8 @@
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    self.dataLoader = [SPTDataLoader dataLoaderWithRequestResponseHandlerDelegate:nil];
+    self.delegate = [SPTDataLoaderRequestResponseHandlerDelegateMock new];
+    self.dataLoader = [SPTDataLoader dataLoaderWithRequestResponseHandlerDelegate:self.delegate];
 }
 
 - (void)tearDown
@@ -32,6 +37,13 @@
 - (void)testNotNil
 {
     XCTAssertNotNil(self.dataLoader, @"The data loader should not be nil after construction");
+}
+
+- (void)testPerformRequestRelayedToRequestResponseHandlerDelegate
+{
+    SPTDataLoaderRequest *request = [SPTDataLoaderRequest new];
+    [self.dataLoader performRequest:request];
+    XCTAssertNotNil(self.delegate.lastRequestPerformed, @"Their should be a valid last request performed");
 }
 
 @end
