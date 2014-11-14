c419adc1addb1d7daa76bd5ca1ad0389d515d35a
Will Sackfield <sackfield@spotify.com>
2014-11-14 13:39:12 +0100

    SPTDataLoader: Add SPTDataLoaderResolverAddressTest

---
+++ SPTDataLoaderTests/SPTDataLoaderResolverAddressTest.m
@@ -0,0 +1,35 @@
+#import <XCTest/XCTest.h>
+
+#import "SPTDataLoaderResolverAddress.h"
+
+@interface SPTDataLoaderResolverAddressTest : XCTestCase
+
+@property (nonatomic, strong) SPTDataLoaderResolverAddress *address;
+
+@end
+
+@implementation SPTDataLoaderResolverAddressTest
+
+#pragma mark XCTestCase
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.address = [SPTDataLoaderResolverAddress dataLoaderResolverAddressWithAddress:@"192.168.0.1"];
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+#pragma mark SPTDataLoaderResolverAddressTest
+
+- (void)testNotNil
+{
+    XCTAssertNotNil(self.address, @"The address should not be nil after construction");
+}
+
+@end
