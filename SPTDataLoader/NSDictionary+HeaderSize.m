045daa0fe5ae350324a6d2e08f6a6dadaece3eab
Will Sackfield <sackfield@spotify.com>
2014-12-20 06:43:02 +1100

    SPTDataLoader: Continue early if key is not string

--- SPTDataLoader/NSDictionary+HeaderSize.m
+++ SPTDataLoader/NSDictionary+HeaderSize.m
@@ -11,15 +11,14 @@
         }
         
         NSString *keyString = (NSString *)key;
-        headerSize += [keyString dataUsingEncoding:NSUTF8StringEncoding].length;
-        headerSize += [@": \n" dataUsingEncoding:NSUTF8StringEncoding].length;
-        
         id object = self[keyString];
         if (![object isKindOfClass:[NSString class]]) {
             continue;
         }
         
         NSString *objectString = (NSString *)object;
+        headerSize += [keyString dataUsingEncoding:NSUTF8StringEncoding].length;
+        headerSize += [@": \n" dataUsingEncoding:NSUTF8StringEncoding].length;
         headerSize += [objectString dataUsingEncoding:NSUTF8StringEncoding].length;
     }
     return headerSize;
