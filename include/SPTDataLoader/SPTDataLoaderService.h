5b0695a7049ffbb054703de8d7fc04aa61c5e400
Patrik Sjöberg <voxar@spotify.com>
2015-07-07 16:01:36 +0200

    remove @param doc line for non existing param

--- include/SPTDataLoader/SPTDataLoaderService.h
+++ include/SPTDataLoader/SPTDataLoaderService.h
@@ -36,8 +36,6 @@
  * @param userAgent The user agent to report as when making HTTP requests
  * @param rateLimiter The limiter for limiting requests per second on a per service basis
  * @param resolver The resolver for rerouting requests to different IP addresses
- * @param consumptionObserver The object tracking the consumption of the service, guaranteed to be called on the main
- * thread
  */
 + (instancetype)dataLoaderServiceWithUserAgent:(NSString *)userAgent
                                    rateLimiter:(SPTDataLoaderRateLimiter *)rateLimiter
