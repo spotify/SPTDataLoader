a8135ba06063c710336bd47c92fba5728d8f5c8a
Jason Gavris <jgavris@spotify.com>
2015-12-22 17:09:47 -0500

    Add slather / coveralls

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'slather',
+  :git      => "https://github.com/viteinfinite/slather",
+  :branch   => "feature-profdata"
---
+++ slather.sh
@@ -0,0 +1,11 @@
+set -euo pipefail
+
+bundle exec slather coverage \
+    --input-format profdata \
+    --cobertura-xml \
+    --ignore "../**/*/Xcode*" \
+    --output-directory slather-report \
+    --scheme SPTDataLoader \
+    SPTDataLoader.xcodeproj
+
+bundle exec slather
