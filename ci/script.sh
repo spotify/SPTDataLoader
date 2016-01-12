33bcd082d055bd1ef4206d2603ab5ea75df64308
Jason Gavris <jgavris@spotify.com>
2016-01-12 15:46:15 -0500

    Test all the platforms available on travis

--- ci/script.sh
+++ ci/script.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 xcrun xcodebuild build test \
     -project SPTDataLoader.xcodeproj \
     -scheme SPTDataLoader \
-    -sdk iphonesimulator \
+    -sdk $TEST_SDK \
+    -destination "platform=iOS Simulator,OS=$OS,name=$NAME" \
     ONLY_ACTIVE_ARCH=YES \
     -enableCodeCoverage YES \
          | xcpretty -c -f `xcpretty-travis-formatter`
