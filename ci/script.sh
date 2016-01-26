8a7d9f052a53d4bbad97e0398c536e2c61fc1783
Aron Cedercrantz <cedercra@spotify.com>
2016-01-26 19:31:23 +0100

    Add source file license validation build job

--- ci/script.sh
+++ ci/script.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
+
 set -euo pipefail
 
+# Executing build actions
+echo "Executing the build actions: $BUILD_ACTIONS"
 xcrun xcodebuild $BUILD_ACTIONS \
-	NSUnbufferedIO=YES \
+    NSUnbufferedIO=YES \
     -project SPTDataLoader.xcodeproj \
     -scheme "$SCHEME" \
     -sdk "$TEST_SDK" \
     -destination "$TEST_DEST" \
     $EXTRA_ARGUMENTS \
-         | xcpretty -c -f `xcpretty-travis-formatter`
+        | xcpretty -c -f `xcpretty-travis-formatter`
 
+
+# License conformance
+./ci/validate_license_conformance.sh {include/SPTDataLoader/*.h,SPTDataLoader/*.{h,m}}
+
+
+# Lint our CocoaPods specification.
 pod spec lint SPTDataLoader.podspec --quick
