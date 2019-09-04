5f62f06b955dac2cd5ccb4dea27be73dad5d39af
Jens Ayton <ahruman@spotify.com>
2019-09-04 10:49:19 +0200

    Adopt NS_ERROR_ENUM

--- include/SPTDataLoader/SPTDataLoaderResponse.h
+++ include/SPTDataLoader/SPTDataLoaderResponse.h
@@ -22,8 +22,10 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+extern NSString * const SPTDataLoaderResponseErrorDomain;
+
 /// http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
-typedef NS_ENUM(NSInteger, SPTDataLoaderResponseHTTPStatusCode) {
+typedef NS_ERROR_ENUM(SPTDataLoaderResponseErrorDomain, SPTDataLoaderResponseHTTPStatusCode) {
     SPTDataLoaderResponseHTTPStatusCodeInvalid = 0,
     // Informational
     SPTDataLoaderResponseHTTPStatusCodeContinue = 100,
@@ -74,8 +76,6 @@ typedef NS_ENUM(NSInteger, SPTDataLoaderResponseHTTPStatusCode) {
 };
 
 @class SPTDataLoaderRequest;
-
-extern NSString * const SPTDataLoaderResponseErrorDomain;
 
 /**
  An object representing the response from the backend
