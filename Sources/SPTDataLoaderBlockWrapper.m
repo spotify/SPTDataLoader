6ae77ae6fcaf3136039b8a397d3842e6c65f15f4
Roy Marmelstein <marmelroy@spotify.com>
2020-07-24 16:58:34 +0200

    SPTDataLoaderBlockWrapper: fix framework build target

--- Sources/SPTDataLoaderBlockWrapper.m
+++ Sources/SPTDataLoaderBlockWrapper.m
@@ -18,7 +18,7 @@
  specific language governing permissions and limitations
  under the License.
  */
-#import "SPTDataLoaderBlockWrapper.h"
+#import <SPTDataLoader/SPTDataLoaderBlockWrapper.h>
 #import "SPTDataLoader.h"
 
 NS_ASSUME_NONNULL_BEGIN
