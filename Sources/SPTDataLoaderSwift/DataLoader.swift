8dec278985d2a184996d0e4a7b5dfb604717d4c0
kmcbride <kmcbride@users.noreply.github.com>
2021-01-15 12:43:33 -0800

    Swift: Clarify request factory method documentation

--- Sources/SPTDataLoaderSwift/DataLoader.swift
+++ Sources/SPTDataLoaderSwift/DataLoader.swift
@@ -23,9 +23,10 @@ import Foundation
 
 /// A protocol that provides data request handling.
 public protocol DataLoader {
-    /// Creates a request that can be used to retrieve the contents of a URL.
+    /// Creates a `Request` that can be used to retrieve the contents of a URL.
     /// - Parameter url: The `URL` for the request.
     /// - Parameter sourceIdentifier: The identifier for the request source. May be `nil`.
+    /// - Returns: A new `Request` instance.
     func request(_ url: URL, sourceIdentifier: String?) -> Request
 }
 
